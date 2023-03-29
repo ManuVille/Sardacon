d64ba595e69e14c57c72ff56ae772cf4ea4ed6b6
Emmanuel Villegas <EmmaVilleAgui@gmail.com>
2023-03-29 17:13:16 -0600

    r5

--- Sardacon/menu.js
+++ Sardacon/menu.js
@@ -1,6 +1,7 @@
 //menu principal
 const btnMenu = document.querySelector("#btnMenu");
 const Menu = document.querySelector("#menu");
+
 btnMenu.addEventListener("click", function(){
     menu.classList.toggle("mostrar")
 });
@@ -15,11 +16,41 @@ for(let i = 0; i < subMenuBtn.length; i++){//dependiendo del boton que presiona.
             if(subMenu.classList.contains("desplegar")){
                 subMenu.classList.remove("desplegar");
                 subMenu.removeAttribute("style");
+                
             } else {
                 subMenu.classList.add("desplegar");
                 subMenu.style.height = height + "px";
+                
             }
 
         }
     });
+}
+
+//mover elementos
+
+const button = document.querySelector("#menu");
+button.addEventListener("click", changeMultipleCSS);
+
+function changeMultipleCSS(e) {
+  // Defining all our CSS styles
+  const myStyles = `
+    margin-top: 45rem;
+  `;
+  const element = document.querySelector(".text__index");
+
+  element.style.cssText = myStyles;
+}
+
+const button1 = document.querySelector("#btnMenu");
+button1.addEventListener("click", changeMultipleCSS1);
+
+function changeMultipleCSS1(e) {
+  // Defining all our CSS styles
+  const myStyles = `
+    margin-top: 5rem;
+  `;
+  const element = document.querySelector(".text__index");
+
+  element.style.cssText = myStyles;
 }
